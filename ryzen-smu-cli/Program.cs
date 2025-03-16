d36decf6db5637826086d3eca84a8823734cbe5e
rawhide kobayashi <rawhide@neet.works>
2025-03-16 03:09:31 -0500

    Release 0.0.1

--- ryzen-smu-cli/Program.cs
+++ ryzen-smu-cli/Program.cs
@@ -11,19 +11,26 @@ class Program
 
         var rootCommand = new RootCommand("A CLI for the Ryzen SMU.");
 
-        var pboOffset = new Option<string>("--offset", "Specify a core, or list of cores, and their PBO offset(s).");
+        var pboOffset = new Option<string>("--offset", "Specify a core, or list of cores, and their PBO offset(s), in a fashion similar to taskset. e.g. 0:-10,1:5,2:-20,14:-25");
 
         rootCommand.AddOption(pboOffset);
-        // How to format this command and process it is to be decided.
-        rootCommand.SetHandler((core_values) =>
+        
+        rootCommand.SetHandler((offsetArgs) =>
         {
-            Console.WriteLine(core_values);
+            RunPBOOffset(offsetArgs);
         }, pboOffset);
 
-        // works
-        ApplySingleCorePBOOffset(0, -15);
-
         return rootCommand.Invoke(args);
+    }
+
+    private static void RunPBOOffset(string offsetArgs)
+    {
+        string[] arg = offsetArgs.Split(',');
+
+        for (int i = 0; i < arg.Length; i++)
+        {
+            ApplySingleCorePBOOffset(Convert.ToInt32(arg[i].Split(':')[0]), Convert.ToInt32(arg[i].Split(':')[1]));
+        }
     }
 
     private static void ApplySingleCorePBOOffset(int coreNumber, int value)
@@ -35,6 +42,7 @@ class Program
         if ((~ryzen.info.topology.coreDisableMap[mapIndex] >> coreNumber % 8 & 1) == 1)
         {
             ryzen.SetPsmMarginSingleCore((uint)(((mapIndex << 8) | coreNumber % 8 & 0xF) << 20), value);
+            Console.WriteLine($"Set core {coreNumber} to offset {value}!");
         }
     }
 }
